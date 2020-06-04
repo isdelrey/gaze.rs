6efbd4b34aba5cc4d543c9f550e40c0f0bfafa05
Ivo Sequeros <ivosequeros@me.com>
2020-06-04 22:12:57 +0200

    Fixed errors

--- client/src/main.rs
+++ client/src/main.rs
@@ -8,13 +8,13 @@ use futures::future::{join_all};
 async fn main() {
     let mut handles = Vec::with_capacity(100);
     /* Run actors: */
-    for _ in 1..100 {
+    for _ in 1..1000 {
         handles.push(tokio::spawn(actor::run()));
     }
 
     join_all(handles).await;
 
-    thread::park()
+    thread::park();
 
     ()
 }
